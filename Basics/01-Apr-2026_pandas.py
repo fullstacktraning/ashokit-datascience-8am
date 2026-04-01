5ac6ff9107361072418c4e3d36b05863692a4108
AshokIT <info@ashokit.in>
2026-04-01 09:02:05 +0530

    code

--- Basics/01-Apr-2026_pandas.py
+++ Basics/01-Apr-2026_pandas.py
@@ -19,8 +19,23 @@ import pandas as pd
 # print(y)
 
 
-# df = pd.read_csv("Book1.csv")
-# print(df)
+df = pd.read_csv("Book1.csv")
+#df = pd.read_csv("Book1.csv",index_col=0,header=None)
+#print(df.head()) #head() - 5rows
+#print(df.tail()) #tail() - last 5 rows
+#print(df.info()) #info() -- give datatype (metadata)
+#print(df.describe()) #describe() -- gives count,std,max,min,.....
+#print(df.shape) #(10,3) (10 rows and 3 cols)
+#print(df.columns) #display only cols
+#print(df.dtypes) #knows the datatypes
 
-df = pd.read_csv("Book1.csv",index_col=0,header=None)
-print(df)
+#print(df["Name"])
+#print(df[["Name","Age"]])
+
+# print(df)
+# print("-------------------------------")
+# print(df.iloc[0]) # by index
+# print("------------------------------")
+# print(df.loc[0]) # by label
+
+# print(df[df["Age"]>30])
